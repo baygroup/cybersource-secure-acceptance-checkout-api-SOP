32b8caebdb62a71eff526630aae87812b64319f1
Bathulah Mahir <bathulah@capitalbay.com.my>
2018-09-24 17:56:34 +0800

    update

--- src/CybersourceSopApi.php
+++ src/CybersourceSopApi.php
@@ -8,30 +8,34 @@ class CybersourceSopApi
     protected $signature_method;
     protected $signed_field_names;
     protected $unsigned_field_names;
+    protected $uniqid ;
+    protected $sign_date_time;
+    protected $locale;
+    protected $profile_id;
+    protected $params;
+    protected $credit_info;
 
-    public function __construct($access_key,$secret_key,$signature_method = 'sha256')
+    public function __construct($access_key,$secret_key,$signature_method = 'sha256',$locale='en')
     {
         $this->access_key = $access_key;
         $this->secret_key = $secret_key;
         $this->signature_method = $signature_method;
+        $this->locale = $locale;
     }
 
 
-    public function sign ($params)
+    public function sign($params)
     {
-        return $this->signData(buildDataToSign($params), $this->secret_key);
+        return signData($this->buildDataToSign($params), $this->secret_key);
     }
 
-    public function signData($data, $secretKey = 'default')
+    function signData($data, $secretKey)
     {
-
-        if($secretKey == 'default') $secretKey = $this->secret_key;
-
-        return \base64_encode(hash_hmac($this->signature_method, $data, $secretKey, true));
+        return base64_encode(hash_hmac($this->signature_method, $data, $secretKey, true));
     }
 
-    public function buildDataToSign(array $params)
-    {
+    public function buildDataToSign($params) {
+
         $signedFieldNames = explode(",",$params["signed_field_names"]);
         foreach ($signedFieldNames as $field) {
             $dataToSign[] = $field . "=" . $params[$field];
@@ -39,55 +43,107 @@ class CybersourceSopApi
         return $this->commaSeparate($dataToSign);
     }
 
+    public function pay($profile_id,array $params)
+    {
+        $profile_id = 'C102A9BC-E1D5-425E-81AE-3603C2A8461B';
+        $this->profile_id = $profile_id;
+        $this->mergeParamsWithProfileId($params);
+        $configArray = $this->generateConfigArray();
+        $this->mergeParamsWithConfig($configArray);
+        $data = $this->generateSignedValue($this->params);// array_map(array($this, 'generateSignedValue'), $this->params, array_keys($this->params));
+
+    }
+
+    public function mergeParamsWithProfileId(array $params)
+    {
+        $this->params = array_merge(['profile_id' => $this->profile_id],$params);
+    }
+
+    public function mergeParamsWithConfig($params)
+    {
+        $this->params = array_merge($this->params ,$params);
+    }
+
+    public function generateConfigArray()
+    {
+        $this->uniqid = uniqid();
+        $this->sign_date_time = gmdate("Y-m-d\TH:i:s\Z");
+        return
+        [
+            'access_key' => $this->access_key,
+            'transaction_uuid' => $this->uniqid,
+            'signed_date_time' => $this->sign_date_time,
+            'locale' => $this->locale
+        ];
+
+    }
+
+    public function generateSignedValue($params)
+    {
+        $this->signed_field_names = '';
+        $this->credit_info = [];
+        $credit_card_information =
+            [
+                'card_type','card_number','card_expiry_date','card_cvn'
+            ];
+
+        foreach($params as $index => $value)
+        {
+
+            if(in_array($index ,$credit_card_information))
+            {
+                $this->credit_info = array_merge($this->credit_info,[$index => $value]);
+                unset($params[$index]);
+                continue;
+            }
+            $this->signed_field_names .= $index.',';
+        }
+
+        $this->params = $params;
+
+        if(count($this->credit_info)) $this->generateUnsignedValue();
+
+        $this->signed_field_names  = substr($this->signed_field_names,0,-1);
+
+        $this->signed_field_names .= ',signed_field_names,unsigned_field_names';
+        $this->params = array_merge($this->params,['signed_field_names' => $this->signed_field_names]);
+
+        $this->params = array_merge($this->params,['unsigned_field_names' => $this->unsigned_field_names]);
+        //do merge credit info at the end of the params later
+
+
+        return $this->signed_field_names;
+    }
+
+    public function generateUnsignedValue()
+    {
+        $this->unsigned_field_names = '';
+        
+        foreach($this->credit_info as $index => $value)
+        {
+            $this->unsigned_field_names .= $index.',';
+        }
+        
+        $this->unsigned_field_names = substr($this->unsigned_field_names,0,-1);
+        
+        return $this->unsigned_field_names;
+    }
+    
+    public function getUnsignedArray()
+    {
+        return $this->credit_info;
+    }
+
+    public function getParams()
+    {
+        return $this->params;
+    }
+
     public function commaSeparate ($dataToSign)
     {
         return implode(",",$dataToSign);
     }
 
-    public function sop_api_key($locale= 'en')
-    {
-        $hidden_input = "<input type='hidden' name='access_key' value='".$this->access_key."'><br>
-                  <input type='hidden' name='transaction_uuid' value='".uniqid()."'><br>
-                  <input type='hidden' name='locale' value='".$locale."'><br>
-                  <input type='hidden' name='signed_date_time' value='".\gmdate("Y-m-d\TH:i:s\Z")."'>
-                 ";
-
-        return $hidden_input;
-    }
-
-    public function generate_signed_data_fields(array $form_fields)
-    {
-        $this->signed_field_names = '';
-
-        $point = 0;
-        foreach($form_fields as $key => $value)
-        {
-            if($point == 0 ) $this->signed_field_names = $key;
-            $this->signed_field_names .= ','.$key;
-            $point ++;
-        }
-
-        $this->signed_field_names = substr($this->signed_field_names, 0,-1);
-        $this->signed_field_names .= $this->signed_field_names .',signed_field_names,unsigned_field_names';
-
-        return $this->signed_field_names;
-    }
-
-    public function generate_unsigned_data_fields(array $form_fields)
-    {
-        $this->unsigned_field_names = '';
-
-        $point = 0;
-        foreach($form_fields as $key => $value)
-        {
-            if($point == 0 ) $this->unsigned_field_names = $key;
-            $this->unsigned_field_names .= ','.$key;
-            $point ++;
-        }
-
-        $this->unsigned_field_names = substr($this->unsigned_field_names, 0,-1);
-
-        return $this->unsigned_field_names;
-    }
+    
 
 }
